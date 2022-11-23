40e84a76873c09513224cad0a8974a8d6f777ec1
AdamLebo <alieberman10@aol.com>
2022-11-23 15:31:09 -0500

    added questions

---
+++ 02-Challenge/Develop/index.js
@@ -0,0 +1,57 @@
+// TODO: Include packages needed for this application
+const inquierer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Write a short description of your project",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Write installation instructions for your project",
+        name: "install",
+    },
+    {
+        type: "input",
+        message: "Write any usage information the user may need to know",
+        name: "usage",
+    },
+    {
+        type: "list",
+        message: "Which license should your project have?",
+        name: "license",
+        choices: [
+            "MIT",
+            "Apache License v2.0",
+            "GNU General Public License v3.0",
+            "None",
+        ]
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
